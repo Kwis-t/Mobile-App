ff49ee885f2f72bd5744fa14d8e36b15ed545bf0
ferdiduisters <ferdiduisters@gmail.com>
2014-06-06 19:38:24 +0200

    Android werkend

--- KwisT/platforms/android/ant-build/HelloWorld-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Applications/MAMP/htdocs/Mobile-App/KwisT/platforms/android/ant-build/HelloWorld-debug-unaligned.apk : \
-/Applications/MAMP/htdocs/Mobile-App/KwisT/platforms/android/ant-build/HelloWorld.ap_ \
-/Applications/MAMP/htdocs/Mobile-App/KwisT/platforms/android/ant-build/classes.dex \
-/Applications/MAMP/htdocs/Mobile-App/KwisT/platforms/android/CordovaLib/ant-build/classes.jar \
