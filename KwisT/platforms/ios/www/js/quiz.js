9248e31f3b65305f4031f9eee702cd89194f817d
ferdiduisters <ferdiduisters@gmail.com>
2014-06-06 13:05:32 +0200

    ff

--- KwisT/platforms/ios/www/js/quiz.js
+++ KwisT/platforms/ios/www/js/quiz.js
@@ -143,7 +143,7 @@ var quizMaster = (function () {
             });
         });
 
-        $( ".deelbuttonclass" ).each(function() {
+        $(".deelbuttonclass" ).each(function() {
             $(this).on("click", function(){
                 alert("test");
             });
